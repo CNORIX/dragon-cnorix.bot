c2847a30e1df23b331d7ba7a42b12399836f0288
DragonFire <64083584+DragonFire1230@users.noreply.github.com>
2022-04-12 22:18:43 +0500

    Add files via upload

---
+++ src/dragonfire-website-7mpl-main/whois/index.php
@@ -0,0 +1,25 @@
+<?php
+echo "Server ip address: ";
+echo $_SERVER['SERVER_ADDR'];
+echo "<br/>";
+echo "Server name: ";
+echo $_SERVER['SERVER_NAME'];
+echo "<br/>";
+echo "Gateway interface: ";
+echo $_SERVER['GATEWAY_INTERFACE'];
+echo "<br/>";
+echo "Request time: ";
+echo $_SERVER['REQUEST_TIME'];
+echo "<br/>";
+echo "Your ip address: ";
+echo $_SERVER['REMOTE_ADDR'];
+echo "<br/>";
+echo "Request method: ";
+echo $_SERVER['REQUEST_METHOD'];
+echo "<br/>";
+echo "HTTP Connection: ";
+echo $_SERVER['HTTP_CONNECTION'];
+echo "<br/>";
+echo "HTTP Host: ";
+echo $_SERVER['HTTP_HOST'];
+?>
